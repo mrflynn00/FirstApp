b351e644b80c1ac8174bdb11b048daa727d4d38c
Saturday Academy <satacad@burger1.local>
2013-04-27 11:07:11 -0700

    Initial Deploment

--- app.rb
+++ app.rb
@@ -2,14 +2,58 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  erb :index   
+  erb :newindex
+end
+
+get '/logins' do
+  @username = params[:username]
+  @password = params[:password]
+  if @username == 'mrflynn00'
+    if @password == 'derp'
+      redirect "/name?name=jpoz&state=Oregon"
+    else
+      @error = "Wrong Password"
+      erb :newindex
+    end
+  else
+    @error = "Wrong username"
+    erb :newindex
+  end
+end
+
+post '/login' do
+  @username = params[:username]
+  @password = params[:password]
+  if @username == 'mrflynn00'
+    if @password == 'derp'
+      erb :home
+    else
+      @error = "Hey dude, you got the password wrong"
+      erb :newindex
+    end
+  else
+    @error = "Wrong username."
+    erb :newindex
+  end
+  if @username == 'nasilemak999'
+    if @password == 'potato'
+      erb :home
+    else
+      @error = "Hey dummy, you got the password wrong"
+      erb :newindex
+    end
+  else
+    @error = "Wrong username."
+    erb :newindex
+  end
 end
 
 get '/name' do
-  @name = (params[:name] || "Nobody").downcase
-  if @name == "collin"
+  @name = (params[:newname] || "Nobody").downcase
+  @state = params[:state]
+  if @name == "aaron"
     redirect 'http://www.youtube.com/watch?v=oHg5SJYRHA0'
   else
-    erb :name
+    erb :newname
   end
 end
